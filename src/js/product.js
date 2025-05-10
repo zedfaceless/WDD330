3f0c01cebb5f59f185d7adcc4364e8edf4550628
zedfaceless <zedfaces.fernandez@gmail.com>
2025-05-11 00:43:17 +0800

    update

--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,27 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
+// Use utility functions to read/write cart
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Get existing cart or start with an empty array
+  const cart = getLocalStorage("so-cart") || [];
+
+  // Add the new product to the cart
+  cart.push(product);
+
+  // Save updated cart
+  setLocalStorage("so-cart", cart);
 }
-// add to cart button event handler
+
+// Event handler for the Add to Cart button
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
 }
 
-// add listener to Add to Cart button
+// Attach event listener
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
